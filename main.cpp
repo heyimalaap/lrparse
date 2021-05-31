f85ddb37b18e1e1cd9e96fddcc6d119c2d6926be
Alaap Surendran <alaapsurendran@gmail.com>
2021-06-01 00:07:29 +0530

    Finish CLR parser

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
+#include <stack>
+#include <map>
+#include <utility>
 
 using namespace std;
+
+#define TOKEN_CASE(chr, tkn) \
+	case chr: \
+		cur = lookahead + 1; \
+		return tkn;
+
 
 enum class Token {
 	ID, PLUS, MULT, BRACKET_OPEN, BRACKET_CLOSE, EOI, ERR
@@ -24,21 +33,11 @@ public:
 
 		int lookahead = cur;
 		switch(input_buffer[lookahead]) {
-			case '+':
-				cur = lookahead + 1;
-				return Token::PLUS;
-			case '*':
-				cur = lookahead + 1;
-				return Token::MULT;
-			case '(':
-				cur = lookahead + 1;
-				return Token::BRACKET_OPEN;
-			case ')':
-				cur = lookahead + 1;
-				return Token::BRACKET_CLOSE;
-			case '\0':
-				cur = lookahead + 1;
-				return Token::EOI;
+			TOKEN_CASE('+', Token::PLUS)
+			TOKEN_CASE('*', Token::MULT)
+			TOKEN_CASE('(', Token::BRACKET_OPEN)
+			TOKEN_CASE(')', Token::BRACKET_CLOSE)
+			TOKEN_CASE('\0', Token::EOI)
 			case 'i':
 				if (input_buffer[++lookahead] == 'd') {
 					cur = lookahead + 1;
@@ -52,6 +51,13 @@ public:
 		}
 	}
 
+	Token peek() {
+		int _cur = cur;
+		Token p = next();
+		cur = _cur;
+		return p;
+	}
+
 private:
 	void error() {
 		cout << "Lex error: " << endl << input_buffer << endl;
@@ -63,5 +69,140 @@ private:
 	string input_buffer;
 };
 
+class Action {
+public:
+	enum ActionType {
+		Shift,
+		Reduce,
+		Accept,
+		Error
+	};
+
+	ActionType type;
+};
+
+class ShiftAction : public Action {
+public:
+	ShiftAction(int shiftState)
+		:shift_state(shiftState)  {
+		type = Action::Shift;
+	}
+	int shift_state;
+};
+
+class ReduceAction : public Action {
+public:
+	ReduceAction(const string& productionLhs, const string& productionRhs)
+		:production_lhs(productionLhs), production_rhs(productionRhs), pop_amt(productionRhs == "id" ? 1 : productionRhs.size())  {
+		type = Action::Reduce;
+	}
+	int pop_amt;
+	string production_lhs;
+	string production_rhs;
+};
+
+class Parser {
+public:
+	Parser(map<pair<int, Token>, Action*> actionMap, map<pair<int, string>, int> gotoMap)
+		:action_map(actionMap), goto_map(gotoMap) {
+		parse_stack.push(0);
+	}
+
+	bool parse(const string& input) {
+		Lexer lex(input);
+		Token a = lex.next();
+		while (true) {
+			int s = parse_stack.top();
+			if (action_map[{s, a}]->type == Action::Shift) {
+				ShiftAction* sa = reinterpret_cast<ShiftAction*>(action_map[{s, a}]);
+				parse_stack.push(sa->shift_state);
+				a = lex.next();
+			} else if (action_map[{s, a}]->type  == Action::Reduce) {
+				ReduceAction* ra = reinterpret_cast<ReduceAction*>(action_map[{s, a}]);
+				for (int i = 0; i < ra->pop_amt; i++) parse_stack.pop();
+				int t = parse_stack.top();
+				if (goto_map[{t, ra->production_lhs}] == -1) {
+					error(a);
+					return false;
+				}
+				parse_stack.push(goto_map[{t, ra->production_lhs}]);
+				cout << "Using production " << ra->production_lhs << " -> " << ra->production_rhs << endl;
+			} else if (action_map[{s, a}]->type == Action::Accept) {
+				return true;
+			} else {
+				error(a);
+				return false;
+			}
+		}
+	}
+private:
+	stack<int> parse_stack;
+	map<pair<int, Token>, Action*> action_map;
+	map<pair<int, string>, int> goto_map;
+
+	void error(Token cur_token) {
+		cout << "Encountered error while parsing" << endl;
+	}
+};
+
+#define ERR_ACTN new Action {.type = Action::Error}
+#define ACC_ACTN new Action {.type = Action::Accept}
+#define SHFT_ACTN(i) new ShiftAction(i)
+#define REDC_ACTN(i) new ReduceAction(productions[i][0], productions[i][1])
+
 int main() {
+	string productions[7][2] = {
+		{"E'", "E"},
+		{"E" , "E+T"},
+		{"E" , "T"},
+		{"T" , "T*F"},
+		{"T" , "F"},
+		{"F" , "(E)"},
+		{"F" , "id"}
+	};
+
+	map<pair<int, Token>, Action*> action_map;
+	map<pair<int, string>, int> goto_map;
+
+	// initialize goto_map
+	for (int i = 0; i < 7; i++)
+		for (int j = 0; j <= 21; j++)
+			goto_map[{j, productions[i][0]}] = -1;
+
+	// Define parse table : Action
+	action_map[{0, Token::PLUS}]  = ERR_ACTN     ; action_map[{0, Token::MULT}]  = ERR_ACTN     ; action_map[{0, Token::BRACKET_OPEN}]  = SHFT_ACTN(4) ; action_map[{0, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{0, Token::ID}]  = SHFT_ACTN(5) ; action_map[{0, Token::EOI}]  = ERR_ACTN    ;
+	action_map[{1, Token::PLUS}]  = SHFT_ACTN(6) ; action_map[{1, Token::MULT}]  = ERR_ACTN     ; action_map[{1, Token::BRACKET_OPEN}]  = ERR_ACTN     ; action_map[{1, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{1, Token::ID}]  = ERR_ACTN     ; action_map[{1, Token::EOI}]  = ACC_ACTN    ;
+	action_map[{2, Token::PLUS}]  = REDC_ACTN(2) ; action_map[{2, Token::MULT}]  = SHFT_ACTN(7) ; action_map[{2, Token::BRACKET_OPEN}]  = ERR_ACTN     ; action_map[{2, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{2, Token::ID}]  = ERR_ACTN     ; action_map[{2, Token::EOI}]  = REDC_ACTN(2);
+	action_map[{3, Token::PLUS}]  = REDC_ACTN(4) ; action_map[{3, Token::MULT}]  = REDC_ACTN(4) ; action_map[{3, Token::BRACKET_OPEN}]  = ERR_ACTN     ; action_map[{3, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{3, Token::ID}]  = ERR_ACTN     ; action_map[{3, Token::EOI}]  = REDC_ACTN(4);
+	action_map[{4, Token::PLUS}]  = ERR_ACTN     ; action_map[{4, Token::MULT}]  = ERR_ACTN     ; action_map[{4, Token::BRACKET_OPEN}]  = SHFT_ACTN(11); action_map[{4, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{4, Token::ID}]  = SHFT_ACTN(11); action_map[{4, Token::EOI}]  = ERR_ACTN    ;
+	action_map[{5, Token::PLUS}]  = REDC_ACTN(6) ; action_map[{5, Token::MULT}]  = REDC_ACTN(6) ; action_map[{5, Token::BRACKET_OPEN}]  = ERR_ACTN     ; action_map[{5, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{5, Token::ID}]  = ERR_ACTN     ; action_map[{5, Token::EOI}]  = REDC_ACTN(6);
+	action_map[{6, Token::PLUS}]  = ERR_ACTN     ; action_map[{6, Token::MULT}]  = ERR_ACTN     ; action_map[{6, Token::BRACKET_OPEN}]  = SHFT_ACTN(4) ; action_map[{6, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{6, Token::ID}]  = SHFT_ACTN(5) ; action_map[{6, Token::EOI}]  = ERR_ACTN    ;
+	action_map[{7, Token::PLUS}]  = ERR_ACTN     ; action_map[{7, Token::MULT}]  = ERR_ACTN     ; action_map[{7, Token::BRACKET_OPEN}]  = SHFT_ACTN(4) ; action_map[{7, Token::BRACKET_CLOSE}]  = ERR_ACTN     ; action_map[{7, Token::ID}]  = SHFT_ACTN(5) ; action_map[{7, Token::EOI}]  = ERR_ACTN    ;
+	action_map[{8, Token::PLUS}]  = SHFT_ACTN(16); action_map[{8, Token::MULT}]  = ERR_ACTN     ; action_map[{8, Token::BRACKET_OPEN}]  = ERR_ACTN     ; action_map[{8, Token::BRACKET_CLOSE}]  = SHFT_ACTN(15); action_map[{8, Token::ID}]  = ERR_ACTN     ; action_map[{8, Token::EOI}]  = ERR_ACTN    ;
+	action_map[{9, Token::PLUS}]  = REDC_ACTN(2) ; action_map[{9, Token::MULT}]  = SHFT_ACTN(17); action_map[{9, Token::BRACKET_OPEN}]  = ERR_ACTN     ; action_map[{9, Token::BRACKET_CLOSE}]  = REDC_ACTN(2) ; action_map[{9, Token::ID}]  = ERR_ACTN     ; action_map[{9, Token::EOI}]  = ERR_ACTN    ;
+	action_map[{10, Token::PLUS}] = REDC_ACTN(4) ; action_map[{10, Token::MULT}] = REDC_ACTN(4) ; action_map[{10, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{10, Token::BRACKET_CLOSE}] = REDC_ACTN(4) ; action_map[{10, Token::ID}] = ERR_ACTN     ; action_map[{10, Token::EOI}] = ERR_ACTN    ;
+	action_map[{11, Token::PLUS}] = ERR_ACTN     ; action_map[{11, Token::MULT}] = ERR_ACTN     ; action_map[{11, Token::BRACKET_OPEN}] = SHFT_ACTN(11); action_map[{11, Token::BRACKET_CLOSE}] = ERR_ACTN     ; action_map[{11, Token::ID}] = SHFT_ACTN(12); action_map[{11, Token::EOI}] = ERR_ACTN    ;
+	action_map[{12, Token::PLUS}] = REDC_ACTN(6) ; action_map[{12, Token::MULT}] = REDC_ACTN(6) ; action_map[{12, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{12, Token::BRACKET_CLOSE}] = REDC_ACTN(6) ; action_map[{12, Token::ID}] = ERR_ACTN     ; action_map[{12, Token::EOI}] = ERR_ACTN    ;
+	action_map[{13, Token::PLUS}] = REDC_ACTN(1) ; action_map[{13, Token::MULT}] = SHFT_ACTN(7) ; action_map[{13, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{13, Token::BRACKET_CLOSE}] = ERR_ACTN     ; action_map[{13, Token::ID}] = ERR_ACTN     ; action_map[{13, Token::EOI}] = REDC_ACTN(1);
+	action_map[{14, Token::PLUS}] = REDC_ACTN(3) ; action_map[{14, Token::MULT}] = REDC_ACTN(3) ; action_map[{14, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{14, Token::BRACKET_CLOSE}] = ERR_ACTN     ; action_map[{14, Token::ID}] = ERR_ACTN     ; action_map[{14, Token::EOI}] = REDC_ACTN(3);
+	action_map[{15, Token::PLUS}] = REDC_ACTN(5) ; action_map[{15, Token::MULT}] = REDC_ACTN(5) ; action_map[{15, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{15, Token::BRACKET_CLOSE}] = ERR_ACTN     ; action_map[{15, Token::ID}] = ERR_ACTN     ; action_map[{15, Token::EOI}] = REDC_ACTN(5);
+	action_map[{16, Token::PLUS}] = ERR_ACTN     ; action_map[{16, Token::MULT}] = ERR_ACTN     ; action_map[{16, Token::BRACKET_OPEN}] = SHFT_ACTN(11); action_map[{16, Token::BRACKET_CLOSE}] = ERR_ACTN     ; action_map[{16, Token::ID}] = SHFT_ACTN(12); action_map[{16, Token::EOI}] = ERR_ACTN    ;
+	action_map[{17, Token::PLUS}] = ERR_ACTN     ; action_map[{17, Token::MULT}] = ERR_ACTN     ; action_map[{17, Token::BRACKET_OPEN}] = SHFT_ACTN(11); action_map[{17, Token::BRACKET_CLOSE}] = ERR_ACTN     ; action_map[{17, Token::ID}] = SHFT_ACTN(12); action_map[{17, Token::EOI}] = ERR_ACTN    ;
+	action_map[{18, Token::PLUS}] = SHFT_ACTN(16); action_map[{18, Token::MULT}] = ERR_ACTN     ; action_map[{18, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{18, Token::BRACKET_CLOSE}] = SHFT_ACTN(21); action_map[{18, Token::ID}] = ERR_ACTN     ; action_map[{18, Token::EOI}] = ERR_ACTN    ;
+	action_map[{19, Token::PLUS}] = REDC_ACTN(1) ; action_map[{19, Token::MULT}] = SHFT_ACTN(17); action_map[{19, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{19, Token::BRACKET_CLOSE}] = REDC_ACTN(1) ; action_map[{19, Token::ID}] = ERR_ACTN     ; action_map[{19, Token::EOI}] = ERR_ACTN    ;
+	action_map[{20, Token::PLUS}] = REDC_ACTN(3) ; action_map[{20, Token::MULT}] = REDC_ACTN(3) ; action_map[{20, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{20, Token::BRACKET_CLOSE}] = REDC_ACTN(3) ; action_map[{20, Token::ID}] = ERR_ACTN     ; action_map[{20, Token::EOI}] = ERR_ACTN    ;
+	action_map[{21, Token::PLUS}] = REDC_ACTN(5) ; action_map[{21, Token::MULT}] = REDC_ACTN(5) ; action_map[{21, Token::BRACKET_OPEN}] = ERR_ACTN     ; action_map[{21, Token::BRACKET_CLOSE}] = REDC_ACTN(5) ; action_map[{21, Token::ID}] = ERR_ACTN     ; action_map[{21, Token::EOI}] = ERR_ACTN    ;
+
+	// Define parse table : Goto
+	goto_map[{0, "E"}] = 1; goto_map[{0, "T"}] = 2; goto_map[{0, "F"}] = 3;
+	goto_map[{4, "E"}] = 8; goto_map[{4, "T"}] = 9; goto_map[{4, "F"}] = 10;
+	goto_map[{6, "T"}] = 13; goto_map[{6, "F"}] = 3;
+	goto_map[{7, "F"}] = 14;
+	goto_map[{11, "E"}] = 18; goto_map[{11, "T"}] = 9; goto_map[{11, "F"}] = 10;
+	goto_map[{16, "T"}] = 19; goto_map[{16, "F"}] = 10;
+	goto_map[{17, "F"}] = 20;
+
+	// Create parser
+	Parser parser(action_map, goto_map);
+	parser.parse("id + id * id");
 }
